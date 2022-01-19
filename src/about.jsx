04bb4c1c09e6549921cd25bf8f37b0e63ffbfc80
miha.novak1024 <miha.novak1024@gmail.com>
2022-01-19 11:28:46 +0100

    Beautify code

--- src/about.jsx
+++ src/about.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
-import { Sidebar } from "./sidebar.jsx";
-import { Header } from "./header.jsx";
-import { Footer } from "./footer.jsx";
 
 export class About extends React.Component {
   render() {
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter, Outlet, Routes, Route } from "react-router-dom";
 import { Sidebar } from "./sidebar.jsx";
